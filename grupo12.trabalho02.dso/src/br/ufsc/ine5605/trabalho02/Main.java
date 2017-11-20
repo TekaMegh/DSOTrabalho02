93c4e9c83b399ee96e7b88876b2fcbd94ef9ac24
Levy Costa <levycosta471@gmail.com>
2017-11-20 02:15:55 -0200

    main changes

--- grupo12.trabalho02.dso/src/br/ufsc/ine5605/trabalho02/Main.java
+++ grupo12.trabalho02.dso/src/br/ufsc/ine5605/trabalho02/Main.java
@@ -25,8 +25,6 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		
-		ControladorCargo.getInstance().incluiCargo("badeco", true , false);
-		
 		ControladorCargo.getInstance().getCargoByNome("badeco").addIntervalo("08:00", "12:00");
 		ControladorCargo.getInstance().getCargoByNome("badeco").addIntervalo("14:00", "18:00");
 
